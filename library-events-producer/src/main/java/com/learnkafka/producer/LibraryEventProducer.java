b6100165d3e6b10eda49aad7df87d5c221f79fd2
Renan Merlo <renan.merlo@paygo.com.br>
2022-02-17 22:46:00 -0300

    Produtor sincrono e assincrono

--- library-events-producer/src/main/java/com/learnkafka/producer/LibraryEventProducer.java
+++ library-events-producer/src/main/java/com/learnkafka/producer/LibraryEventProducer.java
@@ -11,6 +11,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 @Component
 @Slf4j
 public class LibraryEventProducer {
@@ -39,6 +43,30 @@ public class LibraryEventProducer {
         });
 
     }
+
+    public SendResult<Integer,String> sendLibraryEventSynchronous(LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
+
+        Integer key = libraryEvent.getLibraryEventId();
+        String value = objectMapper.writeValueAsString(libraryEvent);
+        SendResult<Integer,String> sendResult = null;
+
+        try {
+            sendResult = kafkaTemplate.sendDefault(key, value).get(1, TimeUnit.SECONDS);
+        } catch (ExecutionException | InterruptedException e) {
+            log.error("ExecutionException/InterruptedException Error sending the message for the key : {} and the value is {}, ERROR: {}",
+                    key, value, e.getMessage());
+            throw e;
+        } catch (Exception e) {
+            log.error("Exception Error sending the message for the key : {} and the value is {}, ERROR: {}",
+                    key, value, e.getMessage());
+            throw e;
+        }
+
+        return sendResult;
+
+    }
+
+
 
     private void handleSuccess(Integer key, String value, SendResult<Integer, String> result) {
         log.info("Message send successfully for the key : {} and the value is {}, partition is {}",
