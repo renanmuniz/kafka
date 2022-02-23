623d1a8573923f38d104c41eaf4d4ba17875079d
Renan Merlo <renan.merlo@paygo.com.br>
2022-02-23 18:13:51 -0300

    Incluido objectmapper para fazer parse da msg em json para dominio

--- library-events-consumer/src/main/java/com/learnkafka/consumer/LibraryEventsConsumer.java
+++ library-events-consumer/src/main/java/com/learnkafka/consumer/LibraryEventsConsumer.java
@@ -1,17 +1,26 @@
 package com.learnkafka.consumer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.learnkafka.domain.LibraryEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
 
 @Component
 @Slf4j
 public class LibraryEventsConsumer {
 
-    @KafkaListener(topics = {"library-events"})
-    public void onMessage(ConsumerRecord<Integer, String> consumerRecord) {
+    @Autowired
+    ObjectMapper objectMapper;
 
+    @KafkaListener(topics = {"library-events"})
+    public void onMessage(ConsumerRecord<Integer, String> consumerRecord) throws JsonProcessingException {
+        LibraryEvent libraryEvent = objectMapper.readValue(consumerRecord.value(), LibraryEvent.class);
+        System.out.println("JSON Parse into domain: " + libraryEvent.toString());
         log.info("ConsumerRecord : {} ", consumerRecord);
 
     }
