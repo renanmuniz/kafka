13af0a358d82566c76da5b5237a7f4d671ecfd3b
Renan Merlo <renan.merlo@paygo.com.br>
2022-02-24 20:47:17 -0300

    Ajustes

--- library-events-consumer/src/main/java/com/learnkafka/consumer/LibraryEventsConsumer.java
+++ library-events-consumer/src/main/java/com/learnkafka/consumer/LibraryEventsConsumer.java
@@ -1,11 +1,8 @@
 package com.learnkafka.consumer;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.learnkafka.domain.LibraryEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -14,13 +11,9 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class LibraryEventsConsumer {
 
-    @Autowired
-    ObjectMapper objectMapper;
-
     @KafkaListener(topics = {"library-events"})
     public void onMessage(ConsumerRecord<Integer, String> consumerRecord) throws JsonProcessingException {
-        LibraryEvent libraryEvent = objectMapper.readValue(consumerRecord.value(), LibraryEvent.class);
-        System.out.println("JSON Parse into domain: " + libraryEvent.toString());
+
         log.info("ConsumerRecord : {} ", consumerRecord);
 
     }
